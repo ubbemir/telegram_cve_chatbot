914a3b091293b7a2e8c1ad10c78f1100d4cdae90
Ubbe <61583055+ubbemir@users.noreply.github.com>
2024-02-27 15:51:38 +0100

    Minor refactor

--- src/logic/interface.rs
+++ src/logic/interface.rs
@@ -5,11 +5,8 @@ use std::error::Error;
 
 pub async fn list_cves(arg: &str) -> Result<String, Box<dyn Error + Send>> {
     let client = NISTAPIClient::new();
-    let response = client.get_cves_from_cpe(arg.to_owned(), true, 10).await;
-    if let Err(e) = response {
-        return Err(e);
-    }
-    let response = response.unwrap();
+    let response = client.get_cves_from_cpe(arg.to_owned(), true, 10).await?;
+
     let result = serde_json::to_string(&response).unwrap();
 
     Ok(result)
